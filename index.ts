3c1e3d6180c1f729f1b097d21086b1a239a66f20
Muhammad Iqbal <iqbal.adudu@gmail.com>
2023-07-10 21:13:54 +0300

    edit index.ts

--- index.ts
+++ index.ts
@@ -51,7 +51,7 @@ async function connectToWhatsApp () {
                         console.log('replying to', msg.key.remoteJid)
                         await sock!.readMessages([msg.key])
                         await sock!.sendMessage(msg.key.remoteJid!, {text: `${custom_data1}`})
-                        await sock!.sendMessage(msg.key.remoteJid!, {text: `${custom_data2}`})
+                        // await sock!.sendMessage(msg.key.remoteJid!, {text: `${custom_data2}`})
                         // await sock!.sendMessage(msg.key.remoteJid!, {text: `${data_tour}`})
                         // await sock!.sendMessage(msg.key.remoteJid!, {text: `${data_services}`})
                     }
