db5030d78fc79cbb45c56a9ef135ea9891128320
Muhammad Iqbal <66395282+Iqbaladudu@users.noreply.github.com>
2023-09-24 15:30:22 +0300

    Update writeData.js

--- writeData.js
+++ writeData.js
@@ -4,18 +4,18 @@ const { promisify } = require('util');
 
 const writeFilePromise = promisify(fs.writeFile);
 
+// (async () => {
+//   const url = 'https://static.staticsave.com/iqbal/bahan-txt.txt';
+//   const response = await axios.get(url);
+//   if (response.data) {
+//     await writeFilePromise('./data/random_satu.txt', response.data);
+//   }
+// })();
+
 (async () => {
   const url = 'https://static.staticsave.com/iqbal/bahan-txt.txt';
   const response = await axios.get(url);
   if (response.data) {
-    await writeFilePromise('./data/random_satu.txt', response.data);
-  }
-})();
-
-(async () => {
-  const url = 'https://static.staticsave.com/iqbal/bahan2-txt.txt';
-  const response = await axios.get(url);
-  if (response.data) {
-    await writeFilePromise('./data/random_dua.txt', response.data);
+    await writeFilePromise('./data/random_lima.txt', response.data);
   }
 })();
